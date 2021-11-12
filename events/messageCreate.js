b32e06924bbe972af2cd3e671fa7daf7a0b4f79e
NhatMinh0208 <minhkhicon2468@gmail.com>
2021-11-12 17:37:21 +0700

    sus

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,6 +5,7 @@ module.exports = {
 	name: 'messageCreate',
 	once: false,
 	async execute(message) {
+        console.log('User ' + message.author.username + ' sent ' + message.content);
         if (findPing.test(message.content)) {
             await message.channel.send('How dare you ping me, @' + message.author.username + '???');
         }
