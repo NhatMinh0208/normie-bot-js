7edc1e3bee206ad7d8bc6baf7cca5c57d54c6995
NhatMinh0208 <minhkhicon2468@gmail.com>
2021-11-12 17:50:43 +0700

    finally

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,12 +1,14 @@
-const findPing = /@NormieBotJS/g;
+const findPing = /<@!908363889492783104>/g;
 
 
 module.exports = {
 	name: 'messageCreate',
 	once: false,
 	async execute(message) {
+        // console.log(message.content);
+        // console.log(message.author.id);
         if (findPing.test(message.content)) {
-            await message.channel.send('How dare you ping me, @' + message.author.username + '???');
+            await message.channel.send('How dare you ping me, <@!' + message.author.id + '>???');
         }
 	},
 };
