ac21f4047871475211d8c71e17af6c7c86016d92
NhatMinh0208 <minhkhicon2468@gmail.com>
2021-11-29 22:16:42 +0700

    google test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const fs = require('fs');
 // Require the necessary discord.js classes
 
+const { Storage } = require('@google-cloud/storage');
 const { Client, Intents } = require('discord.js');
 const token = process.env.DISCORD_TOKEN;
 
@@ -19,6 +20,30 @@ for (const file of eventFiles) {
 		client.on(event.name, async (...args) => event.execute(...args));
 	}
 }
+
+// For more information on ways to initialize Storage, please see
+// https://googleapis.dev/nodejs/storage/latest/Storage.html
+
+const options = {
+    projectId: process.env.GCLOUD_PROJECT_ID,
+    email: process.env.GCLOUD_CLIENT_EMAIL,
+    credentials: {
+        client_email: process.env.GCLOUD_CLIENT_EMAIL,
+        private_key: process.env.GCLOUD_PRIVATE_KEY,
+    },
+};
+
+const storage = new Storage(options);
+
+async function listBuckets() {
+    const [buckets] = await storage.getBuckets();
+    console.log('Buckets:');
+    buckets.forEach(bucket => {
+      console.log(bucket.name);
+    });
+  }
+
+listBuckets().catch(console.error);
 
 // Login to Discord with your client's token
 client.login(token);
